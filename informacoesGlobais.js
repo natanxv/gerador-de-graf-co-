4c105ef64e68d807c0ea12a66030d71424409f3b
natanxv <114235253+natanxv@users.noreply.github.com>
2024-10-15 11:31:22 -0300

    Update informacoesGlobais.js

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -5,7 +5,7 @@ async function vizualizarInformacoesGlobais(){
     const dados = await res.json()
     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
-    const horas = parselnt(dados.tempo_medio)
+    const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio - horas) * 100)
     const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2)
 
